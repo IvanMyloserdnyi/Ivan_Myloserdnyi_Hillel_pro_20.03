c2cb5d73eee9d84f6d8aa6edbdd0f68ab52ba187
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2023-03-23 21:15:48 +0200

    Add files via upload

---
+++ hm_2/script.js
@@ -0,0 +1,12 @@
+
+
+let firstMeaning = prompt();
+let secondMeaning = prompt();
+let thirdMeaning = prompt();
+alert(`${firstMeaning} ${secondMeaning} ${thirdMeaning}`);
+
+let
+    number = 14880;
+    array = ("" + number).split("").join(" ");
+alert(array);
+
