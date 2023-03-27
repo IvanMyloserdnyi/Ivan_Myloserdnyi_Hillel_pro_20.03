3fa1ec7dcd5a545783111d899bb03a46184ac698
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2023-03-27 12:48:40 +0300

    final

--- hm_4/script.js
+++ hm_4/script.js
@@ -2,7 +2,10 @@
 
 let firstNumber = Number(prompt("Give me first number","2"));
 let secondNumber = Number(prompt("Give me second number","2"));
-const calc = firstNumber + secondNumber;
-alert(`${firstNumber}+${secondNumber}=${calc}`);
+const sum = firstNumber + secondNumber;
+const sub = firstNumber - secondNumber;
+const mult = firstNumber * secondNumber;
+const div = firstNumber / secondNumber;
+alert(`${firstNumber}+${secondNumber}=${sum}\n${firstNumber}-${secondNumber}=${sub}\n${firstNumber}*${secondNumber}=${mult}\n${firstNumber}/${secondNumber}=${div}`);
 
 
