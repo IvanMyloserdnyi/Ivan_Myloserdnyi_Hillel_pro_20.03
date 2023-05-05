c856998c116acce6da7bd983ee42f03792a835e4
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2023-05-05 20:52:32 +0300

    .

--- hm_30/script.js
+++ hm_30/script.js
@@ -1,12 +1,6 @@
-<<<<<<< Updated upstream
-const buy = document.querySelector('.buy')
-const form = document.querySelector(".form")
-buy.addEventListener('click', () => {
-=======
 const buyBtn = document.querySelector('.buy')
 const form = document.querySelector(".form")
 buyBtn.addEventListener('click', () => {
->>>>>>> Stashed changes
     form.style.visibility = 'visible'
 })
 
@@ -15,30 +9,6 @@ form.addEventListener('submit',
         const data = Object.fromEntries(new FormData(e.target).entries());
         const values = Object.values(data)
         const keys = Object.keys(data)
-<<<<<<< Updated upstream
-        let a = 0
-        for (let i = 0; i < values.length; i++) {
-            if (values[i].length === 0) {
-                a += 1
-                break
-            }
-        }
-        if (a === 1) {
-            document.write('Incorrect Form!')
-        } else {
-            document.write('About order:<br>')
-            for (let i = 0; i < values.length; i++) {
-                document.write(`${keys[i]}: ${values[i]} <br>`)
-            }
-        }
-        console.log(data)
-
-    })
-
-/*            for (const [key, value] of Object.fromEntries(new FormData(e.target).entries()) {
-                document.write(`${key} ${value}`)
-            }*/
-=======
         for (let i = 0; i < values.length; i++) {
             if (values[i].length === 0) {
                 document.write('Incorrect Form!')
@@ -51,4 +21,3 @@ form.addEventListener('submit',
         }
 
     })
->>>>>>> Stashed changes
