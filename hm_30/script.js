5651a8f13c20d2ec65764b6570b6db6d01a63a20
IvanMyloserdnyi <116162589+IvanMyloserdnyi@users.noreply.github.com>
2023-05-05 20:51:10 +0300

    .

--- hm_30/script.js
+++ hm_30/script.js
@@ -1,6 +1,12 @@
+<<<<<<< Updated upstream
 const buy = document.querySelector('.buy')
 const form = document.querySelector(".form")
 buy.addEventListener('click', () => {
+=======
+const buyBtn = document.querySelector('.buy')
+const form = document.querySelector(".form")
+buyBtn.addEventListener('click', () => {
+>>>>>>> Stashed changes
     form.style.visibility = 'visible'
 })
 
@@ -9,6 +15,7 @@ form.addEventListener('submit',
         const data = Object.fromEntries(new FormData(e.target).entries());
         const values = Object.values(data)
         const keys = Object.keys(data)
+<<<<<<< Updated upstream
         let a = 0
         for (let i = 0; i < values.length; i++) {
             if (values[i].length === 0) {
@@ -31,3 +38,17 @@ form.addEventListener('submit',
 /*            for (const [key, value] of Object.fromEntries(new FormData(e.target).entries()) {
                 document.write(`${key} ${value}`)
             }*/
+=======
+        for (let i = 0; i < values.length; i++) {
+            if (values[i].length === 0) {
+                document.write('Incorrect Form!')
+                return
+            }
+        }
+        document.write('About order:<br>')
+        for (let i = 0; i < values.length; i++) {
+            document.write(`${keys[i]}: ${values[i]} <br>`)
+        }
+
+    })
+>>>>>>> Stashed changes
